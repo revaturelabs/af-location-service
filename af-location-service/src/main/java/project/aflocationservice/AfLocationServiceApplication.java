2235ff888d270ee6cde73479b601db128d03498f
Vijayagopal Krishnan (VJ) <vijay795@outlook.com>
2021-02-23 13:12:07 -0600

    Adding the spring boot project folder

---
+++ af-location-service/src/main/java/project/aflocationservice/AfLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package project.aflocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfLocationServiceApplication.class, args);
+	}
+
+}
