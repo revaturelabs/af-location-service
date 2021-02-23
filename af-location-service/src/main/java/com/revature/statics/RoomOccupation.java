197bcf34b1946585a2cd5bed2378f2c462155e83
Connor Casey <connorcaseyc@gmail.com>
2021-02-23 15:57:29 -0500

    filled out enums

--- af-location-service/src/main/java/com/revature/statics/RoomOccupation.java
+++ af-location-service/src/main/java/com/revature/statics/RoomOccupation.java
@@ -1,6 +1,8 @@
 package com.revature.statics;
 
-public class RoomOccupation {
+public enum RoomOccupation {
 
+    TRAINING,
+    MEETING
 
 }
--- af-location-service/src/main/java/com/revature/statics/RoomType.java
+++ af-location-service/src/main/java/com/revature/statics/RoomType.java
@@ -1,4 +1,9 @@
 package com.revature.statics;
 
 public enum RoomType {
+    
+    VIRTUAL,
+    PHYSICAL,
+    REMOTE
+
 }
