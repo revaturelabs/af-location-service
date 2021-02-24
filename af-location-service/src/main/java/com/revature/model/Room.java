77c7a586e4b68dd145bfdc1853a142282ee22ba5
Michael Romero <michaelromero128@gmail.com>
2021-02-24 15:36:03 -0500

    added instantitation logic

--- af-location-service/src/main/java/com/revature/model/Room.java
+++ af-location-service/src/main/java/com/revature/model/Room.java
@@ -11,5 +11,79 @@ public class Room {
     private RoomOccupation occupation;
     private int capacity;
     private Building building;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the type
+	 */
+	public RoomType getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(RoomType type) {
+		this.type = type;
+	}
+	/**
+	 * @return the occupation
+	 */
+	public RoomOccupation getOccupation() {
+		return occupation;
+	}
+	/**
+	 * @param occupation the occupation to set
+	 */
+	public void setOccupation(RoomOccupation occupation) {
+		this.occupation = occupation;
+	}
+	/**
+	 * @return the capacity
+	 */
+	public int getCapacity() {
+		return capacity;
+	}
+	/**
+	 * @param capacity the capacity to set
+	 */
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	/**
+	 * @return the building
+	 */
+	public Building getBuilding() {
+		return building;
+	}
+	/**
+	 * @param building the building to set
+	 */
+	public void setBuilding(Building building) {
+		this.building = building;
+	}
+    
+    
 
 }
