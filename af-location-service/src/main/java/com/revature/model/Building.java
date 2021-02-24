73fe2f1b5472294bdf032283b904ad997772c36f
Michael Romero <michaelromero128@gmail.com>
2021-02-24 15:36:03 -0500

    added instantitation logic

--- af-location-service/src/main/java/com/revature/model/Building.java
+++ af-location-service/src/main/java/com/revature/model/Building.java
@@ -2,137 +2,72 @@ package com.revature.model;
 
 import java.util.List;
 
-import javax.persistence.*;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@Entity
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Building {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="building_id")
-    private int buildingId;
-	
+    private int id;
     private String city;
-    
-    @Column(name = "street_address")
     private String streetAddress;
-    
-    @ManyToOne(fetch = FetchType.EAGER)
     private Location location;
-    
-    @OneToMany
     private List<Room> rooms;
-    
-    public Building() {
-    	
-    }
-    
-	public Building(int buildingId, String city, String streetAddress, Location location, List<Room> rooms) {
-		super();
-		this.buildingId = buildingId;
-		this.city = city;
-		this.streetAddress = streetAddress;
-		this.location = location;
-		this.rooms = rooms;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
 	}
-
-	public int getBuildingId() {
-		return buildingId;
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
 	}
-
-	public void setBuildingId(int buildingId) {
-		this.buildingId = buildingId;
-	}
-
+	/**
+	 * @return the city
+	 */
 	public String getCity() {
 		return city;
 	}
-
+	/**
+	 * @param city the city to set
+	 */
 	public void setCity(String city) {
 		this.city = city;
 	}
-
+	/**
+	 * @return the streetAddress
+	 */
 	public String getStreetAddress() {
 		return streetAddress;
 	}
-
+	/**
+	 * @param streetAddress the streetAddress to set
+	 */
 	public void setStreetAddress(String streetAddress) {
 		this.streetAddress = streetAddress;
 	}
-
+	/**
+	 * @return the location
+	 */
 	public Location getLocation() {
 		return location;
 	}
-
+	/**
+	 * @param location the location to set
+	 */
 	public void setLocation(Location location) {
 		this.location = location;
 	}
-
+	/**
+	 * @return the rooms
+	 */
 	public List<Room> getRooms() {
 		return rooms;
 	}
-
+	/**
+	 * @param rooms the rooms to set
+	 */
 	public void setRooms(List<Room> rooms) {
 		this.rooms = rooms;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + buildingId;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((location == null) ? 0 : location.hashCode());
-		result = prime * result + ((rooms == null) ? 0 : rooms.hashCode());
-		result = prime * result + ((streetAddress == null) ? 0 : streetAddress.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Building other = (Building) obj;
-		if (buildingId != other.buildingId)
-			return false;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		} else if (!city.equals(other.city))
-			return false;
-		if (location == null) {
-			if (other.location != null)
-				return false;
-		} else if (!location.equals(other.location))
-			return false;
-		if (rooms == null) {
-			if (other.rooms != null)
-				return false;
-		} else if (!rooms.equals(other.rooms))
-			return false;
-		if (streetAddress == null) {
-			if (other.streetAddress != null)
-				return false;
-		} else if (!streetAddress.equals(other.streetAddress))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Building [buildingId=" + buildingId + ", city=" + city + ", streetAddress=" + streetAddress
-				+ ", location=" + location + ", rooms=" + rooms + "]";
-	}
-
-	
-    
-    
-    
 }
