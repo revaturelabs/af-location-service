dd1acd4883c307cd4b9f5232e44b1246c3adc928
Jesus Cabrero <cabre@192.168.0.26>
2021-03-03 14:31:42 -0500

    removed location controller test

--- af-location-service/src/test/java/com/revature/controller/LocationControllerTests.java
+++ af-location-service/src/test/java/com/revature/controller/LocationControllerTests.java
@@ -12,11 +12,4 @@ import com.revature.service.LocationService;
 public class LocationControllerTests {
 	
 	private LocationService ls;	
-	
-	@Test
-	public void checkGetBuildingsAtLocationController() {
-		List<BuildingDto> list =  ls.getAllBuildings();
-		assertNotNull(list);
-		assertEquals(list.getClass().equals(BuildingDto.class),"building classes are not the same");
-	}
 }
