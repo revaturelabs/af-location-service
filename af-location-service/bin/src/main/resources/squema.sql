ec01aa2d9b9dc19896ad64b141da2e6d59d978b5
Jesus Cabrero <cabre@192.168.0.26>
2021-03-05 07:09:36 -0500

    changed to yaml file, added drop if to schema

--- af-location-service/bin/src/main/resources/squema.sql
+++ af-location-service/bin/src/main/resources/squema.sql
@@ -1,8 +1,3 @@
-drop table location if exists;
-drop table room if exists;
-drop table building if exists;
-drop table room_type if exists;
-dorp table room_occupation if exists;
 
 create table location(
 	location_id serial primary key, 
