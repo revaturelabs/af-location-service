6f87e48f6a1f82fa8c9212ec1824c5146698a085
Vijayagopal Krishnan (VJ) <vijay795@outlook.com>
2021-03-08 14:56:37 -0600

    Added implementations for Creating Updating and Deletion of Locations for LocationController and LocationService.

--- af-location-service/src/main/java/com/revature/service/LocationServiceImpl.java
+++ af-location-service/src/main/java/com/revature/service/LocationServiceImpl.java
@@ -2,6 +2,7 @@ package com.revature.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.revature.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +10,30 @@ import org.springframework.stereotype.Service;
 
 import com.revature.model.Building;
 import com.revature.model.Location;
-import com.revature.repository.BuildingRepository;
 import com.revature.repository.LocationRepository;
 
 @Service
 public class LocationServiceImpl implements LocationService{
-	
-	private LocationRepository locationRepository;
-	
+
 	@Autowired
-	public LocationServiceImpl(LocationRepository locationRepository) {
-
-		this.locationRepository=locationRepository;
-
-	}
+	private LocationRepository locationRepository;
 
 	@Override
-	public void createLocation(LocationRequestDto locationRequestDto) {
-		// TODO Auto-generated method stub
+	public void createLocation( LocationRequestDto locationRequestDto ) {
+
+		Location location = new Location();
+		location.setCity( locationRequestDto.getCity() );
+		location.setState(locationRequestDto.getState());
+		location.setZipCode(locationRequestDto.getZipCode());
+		locationRepository.save(location);
 		
 	}
 
 	@Override
 	public List<LocationDto> getAllLocations() {
-		// TODO Auto-generated method stub
+
 		return null;
+
 	}
 
 	@Override
@@ -62,37 +62,60 @@ public class LocationServiceImpl implements LocationService{
 
 	@Override
 	public void updateState(int index, String state) {
-		// TODO Auto-generated method stub
+
+		Location location = locationRepository.findById(index).get();
+		location.setState(state);
+		locationRepository.save(location);
 		
 	}
 
 	@Override
 	public void updateCity(int index, String city) {
-		// TODO Auto-generated method stub
+
+		Location location = locationRepository.findById(index).get();
+		location.setCity(city);
+		locationRepository.save(location);
 		
 	}
 
 	@Override
 	public void updateZipCode(int index, String zipCode) {
-		// TODO Auto-generated method stub
+
+		Location location = locationRepository.findById(index).get();
+		location.setZipCode(zipCode);
+		locationRepository.save(location);
 		
 	}
 
 	@Override
 	public void deleteLocation(int index) {
-		// TODO Auto-generated method stub
-		
+
+		locationRepository.deleteById(index);
+
 	}
 
 	@Override
 	public void addBuilding(int index, BuildingRequestDto buildingRequestDto) {
-		// TODO Auto-generated method stub
+
+		Location location = locationRepository.findById(index).get();
+		Building building = new Building();
+		building.setLocation(location);
+		building.setTotalFloors(buildingRequestDto.getTotalFloors());
+		building.setStreetAddress(buildingRequestDto.getStreet_address());
+		building.setCity(buildingRequestDto.getCity());
+		location.getBuildings().add(building);
+		locationRepository.save(location);
 		
 	}
 
 	@Override
 	public void updateLocation(int index, LocationRequestDto locationRequestDto) {
-		// TODO Auto-generated method stub
+
+		Location location = locationRepository.findById(index).get();
+		location.setState(locationRequestDto.getState());
+		location.setCity(locationRequestDto.getCity());
+		location.setZipCode(locationRequestDto.getZipCode());
+		locationRepository.save(location);
 		
 	}
 
