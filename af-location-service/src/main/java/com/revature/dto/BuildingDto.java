22e026b87b3666186ededdf3f6ac39e4d6352d0e
Vijayagopal Krishnan (VJ) <vijay795@outlook.com>
2021-03-10 23:37:18 -0600

    Update BuildingDto.java
    Fixed the compilation errors for BuildingDto caused by the merge conflict

--- af-location-service/src/main/java/com/revature/dto/BuildingDto.java
+++ af-location-service/src/main/java/com/revature/dto/BuildingDto.java
@@ -3,50 +3,6 @@ package com.revature.dto;
 
 public class BuildingDto {
 
-	public BuildingDto() {
-
-	}
-
-	public int getId() {
-
-		return id;
-	}
-
-	public void setId(int id) {
-
-		this.id = id;
-	}
-
-	public String getStreet_address() {
-
-		return street_address;
-	}
-
-	public void setStreet_address(String street_address) {
-
-		this.street_address = street_address;
-	}
-
-	public int getTotalFloors() {
-
-		return totalFloors;
-	}
-
-	public void setTotalFloors(int totalFloors) {
-
-		this.totalFloors = totalFloors;
-	}
-
-	public int getNumRooms() {
-
-		return numRooms;
-	}
-
-	public void setNumRooms(int numRooms) {
-
-		this.numRooms = numRooms;
-	}
-
 	private int id;
 	private String street_address;
 	private int totalFloors;
@@ -65,6 +21,7 @@ public class BuildingDto {
 				", totalFloors=" + totalFloors +
 				", numRooms=" + numRooms +
 				'}';
+	
 	}
 
 	public int getId() {
@@ -113,6 +70,7 @@ public class BuildingDto {
 		this.street_address = street_address;
 		this.totalFloors = totalFloors;
 		this.numRooms = numRooms;
+		
 	}
 
 }
