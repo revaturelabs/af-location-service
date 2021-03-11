7648f61cc973753c31302daf4eb93f47287f3021
Vijayagopal Krishnan (VJ) <vijay795@outlook.com>
2021-03-10 23:33:48 -0600

    Update LocationRequestDto.java
    Fixed the compilation errors caused by the merge request

--- af-location-service/src/main/java/com/revature/dto/LocationRequestDto.java
+++ af-location-service/src/main/java/com/revature/dto/LocationRequestDto.java
@@ -2,36 +2,6 @@ package com.revature.dto;
 
 public class LocationRequestDto {
 
-    public String getState() {
-
-        return state;
-    }
-
-    public void setState(String state) {
-
-        this.state = state;
-    }
-
-    public String getCity() {
-
-        return city;
-    }
-
-    public void setCity(String city) {
-
-        this.city = city;
-    }
-
-    public String getZipCode() {
-
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-
-        this.zipCode = zipCode;
-    }
-
     private String state;
     private String city;
     private String zipCode;
