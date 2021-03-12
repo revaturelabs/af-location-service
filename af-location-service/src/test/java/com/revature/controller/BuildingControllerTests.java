410c0d4c3aeae54a01a18e202e28a4a602775eb7
Ulric Todman <rick.sinclair.nyc@gmail.com>
2021-03-12 13:29:26 -0500

    pulled from main

--- af-location-service/src/test/java/com/revature/controller/BuildingControllerTests.java
+++ af-location-service/src/test/java/com/revature/controller/BuildingControllerTests.java
@@ -19,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(BuildingController.class)
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 public class BuildingControllerTests {
