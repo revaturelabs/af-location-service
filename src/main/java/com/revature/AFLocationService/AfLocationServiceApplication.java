a97b46bd02902838b0efce27acf4f919e07a213b
samaurer2 <43677441+samaurer2@users.noreply.github.com>
2021-03-30 17:00:30 -0500

    Initial Skeleton Commit

---
+++ src/main/java/com/revature/AFLocationService/AfLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.AFLocationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfLocationServiceApplication.class, args);
+	}
+
+}
