d7bcea752c9bd2b2a9e4606d38c1987e3eb3e0c9
MGlennUA <62678834+MGlennUA@users.noreply.github.com>
2021-03-31 14:51:50 -0700

    Services getter setter
    Added getter and setter for repos to the services

--- src/main/java/com/revature/services/LocationServiceImpl.java
+++ src/main/java/com/revature/services/LocationServiceImpl.java
@@ -1,10 +1,26 @@
 package com.revature.services;
 
 
+import com.revature.repos.LocationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component
 @Service
 public class LocationServiceImpl {
+
+    @Autowired
+    LocationRepo locationRepo;
+
+    public LocationServiceImpl() {
+    }
+
+    public LocationRepo getLocationRepo() {
+        return locationRepo;
+    }
+
+    public void setLocationRepo(LocationRepo locationRepo) {
+        this.locationRepo = locationRepo;
+    }
 }
