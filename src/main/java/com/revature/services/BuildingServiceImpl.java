b4addaa5fe7cb69e82cad79325b0da1a7162e10b
MGlennUA <62678834+MGlennUA@users.noreply.github.com>
2021-03-31 14:47:02 -0700

    BuildingServiceTests Mocks
    Started mocking the building service tests

--- src/main/java/com/revature/services/BuildingServiceImpl.java
+++ src/main/java/com/revature/services/BuildingServiceImpl.java
@@ -1,9 +1,25 @@
 package com.revature.services;
 
+import com.revature.repos.BuildingRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component
 @Service
 public class BuildingServiceImpl {
+
+    @Autowired
+    BuildingRepo buildingRepo;
+
+    public BuildingServiceImpl() {
+    }
+
+    public BuildingRepo getBuildingRepo() {
+        return buildingRepo;
+    }
+
+    public void setBuildingRepo(BuildingRepo buildingRepo) {
+        this.buildingRepo = buildingRepo;
+    }
 }
--- src/test/java/com/revature/services/BuildingServiceTests.java
+++ src/test/java/com/revature/services/BuildingServiceTests.java
@@ -1,4 +1,26 @@
 package com.revature.services;
 
+import com.revature.AFLocationService.AfLocationServiceApplication;
+import com.revature.repos.BuildingRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest(classes = AfLocationServiceApplication.class)
+@ExtendWith(MockitoExtension.class)
 public class BuildingServiceTests {
+
+    BuildingService buildingService;
+
+    @MockBean
+    BuildingRepo buildingrepo;
+
+    @Test
+    void testIfTestsCanRun() {
+        Assertions.assertTrue(true);
+    }
 }
